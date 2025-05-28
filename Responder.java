bf3e704570cf17eca12617bdb32a31e48ec5a15c
nurinsofina <nrn2608@gmail.com>
2025-05-28 22:22:44 +0800

    Add files via upload

---
+++ Responder.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author nurinsofina
+ */
+public abstract class Responder {
+    // Variables
+    protected String responderID;
+    protected String responderName;
+    protected String contactNumber;
+    
+    // Constructor
+    public Responder (String responderID, String responderName, String contactNumber) {
+        this.responderID = responderID;
+        this.responderName = responderName;
+        this.contactNumber = contactNumber;
+    }
+    
+    // Methids
+    public void dispatchResponder() {
+        System.out.println("Dispatching responder: " + responderName + 
+                " (ID: " + responderID + ") ");
+    }
+    
+    public void updateStatus(String status) {
+        System.out.println("Responder " + responderName + " updated status: " + status);
+    }
+    
+    // Polymorphic method to describe the responder
+    public abstract void describeRole();
+    
+}
