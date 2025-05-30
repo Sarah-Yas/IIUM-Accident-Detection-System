accd5491b09cd9684ea014b4864716766083e6e3
Sarah-Yas <sarahyasmin106@gmail.com>
2025-05-30 22:20:49 +0800

    Update MainMenu.java

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -4,26 +4,16 @@
  */
 package src.view;
 
+import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.geometry.Pos;
-import src.view.LoginWindow;
 
-public class MainMenu {
-    public void start(Stage stage) {
-        VBox root = new VBox(15);
-        root.setAlignment(Pos.CENTER);
+public class MainMenu extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        LoginWindow.display(primaryStage);
+    }
 
-        Button userLoginBtn = new Button("User Login");
-        userLoginBtn.setOnAction(e -> new LoginWindow("user").start(stage));
-
-        root.getChildren().addAll(userLoginBtn);
-
-        Scene scene = new Scene(root, 400, 300);
-        stage.setTitle("Accident Detection System - Main Menu");
-        stage.setScene(scene);
-        stage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 }
