18a74a99502b704c2873220f7e6080d88164db6f
NurBalqis05 <nurbalqis0509@gmail.com>
2025-05-30 20:07:05 +0800

    Add files via upload

---
+++ src/main/java/CallAlert.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.alertdispatcher;
+
+/**
+ *
+ * @author User
+ */
+public class CallAlert extends AlertDispatcher {
+    private String phoneNumber;
+    private int callDuration;
+    
+    //Constructor
+    public CallAlert(int dispatcherID, String dispatcherName,String phoneNumber,int callDuration){
+        super(dispatcherID,dispatcherName);
+        this.phoneNumber=phoneNumber;
+        this.callDuration=callDuration;
+    }
+    
+    //Getter and Setter
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public int getCallDuration() {
+        return callDuration;
+    }
+
+    public void setCallDuration(int callDuration) {
+        this.callDuration = callDuration;
+    }
+
+    //Method
+    public void MakeEmergencyCall(){
+        System.out.println("Dialing emergency number: " + getPhoneNumber());
+    }
+    public void EndCall(){
+        System.out.println("Call ended. Duration: " + getCallDuration() + " seconds");
+    }
+    
+    @Override
+    public void sendAlert() {
+    MakeEmergencyCall();
+    EndCall();
+    updateAlertStatus("Completed");
+}
+}
