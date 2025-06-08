2ba3b325f3dc671f7a6ee24ca98b368dd504d880
Sarah-Yas <sarahyasmin106@gmail.com>
2025-06-08 20:26:46 +0800

    Create CameraSensor.java

---
+++ src/sensor/CameraSensor.java
@@ -0,0 +1,22 @@
+package sensor;
+
+public class CameraSensor extends Sensor {
+    protected boolean status;
+
+    public CameraSensor(int sensorID, String location, String impactData, boolean status) {
+        super(sensorID, location, impactData);
+        this.status = status;
+    }
+
+    public void detectFromCamera() {
+        System.out.println("Camera detected movement at " + location);
+    }
+
+    public void captureImage() {
+        System.out.println("Capturing image at " + location);
+    }
+
+    public void sendDataToAIEngine() {
+        System.out.println("Sending image data to AI Engine...");
+    }
+}
