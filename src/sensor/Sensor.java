ccffbac4865b09d860f4be93b27204f537d13424
Sarah-Yas <sarahyasmin106@gmail.com>
2025-06-08 20:27:00 +0800

    Create Sensor.java

---
+++ src/sensor/Sensor.java
@@ -0,0 +1,21 @@
+package sensor;
+
+public class Sensor {
+    protected int sensorID;
+    protected String location;
+    protected String impactData;
+
+    public Sensor(int sensorID, String location, String impactData) {
+        this.sensorID = sensorID;
+        this.location = location;
+        this.impactData = impactData;
+    }
+
+    public void detectImpact() {
+        System.out.println("Impact detected at " + location);
+    }
+
+    public void sendImpactData() {
+        System.out.println("Sending impact data: " + impactData);
+    }
+}
